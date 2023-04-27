7b9b15f49ccc4dc793012ac49543ede1114568ef
Theodor Blom <me@theodorblom.com>
2023-04-27 22:09:52 +0200

    Improving Chamber class

--- src/dungen/dungeon.py
+++ src/dungen/dungeon.py
@@ -27,7 +27,17 @@ class Chamber:
     def __init__(self, origin_direction: helpers.CardinalDirection):
         self.origin_direction = origin_direction
         self.dimensions: dict = self.get_dimensions()
-        self.pathways: dict[list] = self.get_pathways()
+        self.exits: dict[list] = self.get_exits()
+
+    def __repr__(self):
+        return f"Chamber({self.origin_direction})"
+
+    def __str__(self):
+        return ("Chamber:\n"
+                f"\t{self.origin_direction=}\n"
+                f"\t{self.dimensions=}\n"
+                f"\t{self.exits=}\n"
+                )
 
     def get_dimensions(self) -> dict:
         """
@@ -39,37 +49,37 @@ class Chamber:
             "size": None
         }
         rand: int = helpers.rolldice(19)
-        if rand in list(range(1, 3)):
+        if rand in helpers.rangeroll(1, 2):
             dimensions["shape"] = "square"
             dimensions["size"] = (20, 20)
-        elif rand in list(range(3, 5)):
+        elif rand in helpers.rangeroll(3, 4):
             dimensions["shape"] = "square"
             dimensions["size"] = (30, 30)
-        elif rand in list(range(5, 7)):
+        elif rand in helpers.rangeroll(5, 6):
             dimensions["shape"] = "square"
             dimensions["size"] = (40, 40)
-        elif rand in list(range(7, 10)):
+        elif rand in helpers.rangeroll(7, 9):
             dimensions["shape"] = "rectangle"
             dimensions["size"] = (20, 30)
-        elif rand in list(range(10, 13)):
+        elif rand in helpers.rangeroll(10, 12):
             dimensions["shape"] = "rectangle"
             dimensions["size"] = (30, 40)
-        elif rand in list(range(13, 15)):
+        elif rand in helpers.rangeroll(13, 14):
             dimensions["shape"] = "rectangle"
             dimensions["size"] = (40, 50)
-        elif rand in list(range(15, 16)):
+        elif rand in helpers.rangeroll(15, 15):
             dimensions["shape"] = "rectangle"
             dimensions["size"] = (50, 80)
-        elif rand in list(range(16, 17)):
+        elif rand in helpers.rangeroll(16, 16):
             dimensions["shape"] = "circle"
             dimensions["size"] = (30, 30)
-        elif rand in list(range(17, 18)):
+        elif rand in helpers.rangeroll(17, 17):
             dimensions["shape"] = "circle"
             dimensions["size"] = (50, 50)
-        elif rand in list(range(18, 19)):
+        elif rand in helpers.rangeroll(18, 18):
             dimensions["shape"] = "octagon"
             dimensions["size"] = (40, 40)
-        elif rand in list(range(19, 20)):
+        elif rand in helpers.rangeroll(19, 19):
             dimensions["shape"] = "octagon"
             dimensions["size"] = (60, 60)
         else:
@@ -77,58 +87,70 @@ class Chamber:
 
         return dimensions
 
-    def get_pathways(self) -> dict[list]:
+    def get_exits(self) -> dict[list]:
         """
         Method returning a dictionary mapping each cordinal direction to a list
-        of pathways.
+        of exits.
         """
-        pathways = {
+        exits = {
             "North": [],
             "East": [],
             "South": [],
             "West": []
         }
-        # TODO: Append Door objects not strings
 
         # There is always an origin pathway entering a chamber.
-        pathways[str(self.origin_direction.turn(180))].append("origin")
+        exits[str(self.origin_direction.turn(180))].append("origin")
 
         # Generate a random number of pathways in each cordinal direction
-        num_pathways = self.get_num_pathways()
-        for _ in range(num_pathways):
+        num_exits = self.get_num_exits()
+        for _ in range(num_exits):
             rand = helpers.rolldice(20)
-            if rand in list(range(1, 8)):
-                direction: str = str(self.origin_direction)
-                pathways[direction].append("door")
-            elif rand in list(range(8, 13)):
-                direction: str = str(self.origin_direction.turn(-90))
-                pathways[direction].append("door")
-            elif rand in list(range(13, 18)):
-                direction: str = str(self.origin_direction.turn(90))
-                pathways[direction].append("door")
-            elif rand in list(range(18, 21)):
-                direction: str = str(self.origin_direction.turn(180))
-                pathways[direction].append("door")
+            if rand in helpers.rangeroll(1, 7):
+                direction = self.origin_direction
+                exits[str(direction)].append(self.get_exit_type(direction))
+            elif rand in helpers.rangeroll(8, 12):
+                direction = self.origin_direction.turn(-90)
+                exits[str(direction)].append(self.get_exit_type(direction))
+            elif rand in helpers.rangeroll(13, 17):
+                direction = self.origin_direction.turn(90)
+                exits[str(direction)].append(self.get_exit_type(direction))
+            elif rand in helpers.rangeroll(18, 20):
+                direction = self.origin_direction.turn(180)
+                exits[str(direction)].append(self.get_exit_type(direction))
             else:
                 raise ValueError(f"Invalid diceroll: {rand}")
-        return pathways
+        return exits
 
-    def get_num_pathways(self) -> int:
+    def get_num_exits(self) -> int:
         """
-        Method for obtaining the number of pathways in a chamber.
+        Method for obtaining the number of exits in a chamber.
         Can be zero since the origin pathway is added elsewhere.
         """
         rand = helpers.rolldice(20)
-        if rand in list(range(1, 6)):
-            num_pathways = 0
-        elif rand in list(range(6, 12)):
-            num_pathways = 1
-        elif rand in list(range(12, 16)):
-            num_pathways = 2
-        elif rand in list(range(16, 19)):
-            num_pathways = 3
-        elif rand in list(range(19, 21)):
-            num_pathways = 4
+        if rand in helpers.rangeroll(1, 5):
+            num_exits = 0
+        elif rand in helpers.rangeroll(6, 11):
+            num_exits = 1
+        elif rand in helpers.rangeroll(12, 15):
+            num_exits = 2
+        elif rand in helpers.rangeroll(16, 18):
+            num_exits = 3
+        elif rand in helpers.rangeroll(19, 20):
+            num_exits = 4
         else:
             raise ValueError(f"Invalid diceroll: {rand}")
-        return num_pathways
+        return num_exits
+
+    def get_exit_type(self, direction) -> Passage | Door:
+        """
+        Method returning the type of exit leading from a Chamber.
+        Can either be a Passage or a Door object.
+        """
+        rand = helpers.rolldice(20)
+        if rand in helpers.rangeroll(1, 10):
+            return Door(direction)
+        elif rand in helpers.rangeroll(11, 20):
+            return Passage()
+        else:
+            raise ValueError(f"Invalid diceroll: {rand}")
